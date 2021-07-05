88c58671b392c9c9aefc45546fa9a92bb904778a
hellodebs <debora.behrens@outlook.com>
2021-07-05 09:16:05 +0200

    Add toggable bookmark.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const showAnswerButtonElements =
   document.querySelectorAll("button.show-answer");
 const answerElements = document.querySelectorAll(".answers");
 
+//Hide and show answer
+
 for (let i = 0; i < answerElements.length; i++) {
   const currentButton = showAnswerButtonElements[i];
   const currentAnswer = answerElements[i];
@@ -11,6 +13,20 @@ for (let i = 0; i < answerElements.length; i++) {
       currentButton.textContent = "Hide Answer";
     } else {
       currentButton.textContent = "Show Answer";
+    }
+  });
+}
+
+// toggle bookmark
+
+const bookmarkButtons = document.querySelectorAll(".bookmarks");
+for (let i = 0; i < bookmarkButtons.length; i++) {
+  const currentBookmark = bookmarkButtons[i];
+  currentBookmark.addEventListener("click", () => {
+    if (currentBookmark.classList.contains("far")) {
+      currentBookmark.classList.replace("far", "fas");
+    } else {
+      currentBookmark.classList.replace("fas", "far");
     }
   });
 }
