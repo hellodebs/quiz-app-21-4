3f0d6c4b2f5a5d0c2cf815783bfae361e8f753c9
hellodebs <debora.behrens@outlook.com>
2021-07-08 10:34:59 +0200

    Add bookmark.html and .js, linking bookmark section

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import { createCardComponent } from "./lib/card.js";
 import { data } from "./lib/db.js";
 
+/* Iterate over the data array, create a card using the createCardComponent 
+function and append it to the element (in my case section) that is supposed 
+to hold the quiz question cards */
+
 const sectionElement = document.querySelector("section.quiz-cards");
 
 data.forEach((card) => {
@@ -15,29 +19,29 @@ const answerElements = document.querySelectorAll(".answers");
 
 //Hide and show answer
 
-for (let i = 0; i < answerElements.length; i++) {
-  const currentButton = showAnswerButtonElements[i];
-  const currentAnswer = answerElements[i];
-  currentButton.addEventListener("click", () => {
-    currentAnswer.classList.toggle("hidden");
-    if (currentButton.textContent === "Show Answer") {
-      currentButton.textContent = "Hide Answer";
-    } else {
-      currentButton.textContent = "Show Answer";
-    }
-  });
-}
+// for (let i = 0; i < answerElements.length; i++) {
+//   const currentButton = showAnswerButtonElements[i];
+//   const currentAnswer = answerElements[i];
+//   currentButton.addEventListener("click", () => {
+//     currentAnswer.classList.toggle("hidden");
+//     if (currentButton.textContent === "Show Answer") {
+//       currentButton.textContent = "Hide Answer";
+//     } else {
+//       currentButton.textContent = "Show Answer";
+//     }
+//   });
+// }
 
 //  toggable bookmark
 
-const bookmarkButtons = document.querySelectorAll(".bookmarks");
-for (let i = 0; i < bookmarkButtons.length; i++) {
-  const currentBookmark = bookmarkButtons[i];
-  currentBookmark.addEventListener("click", () => {
-    if (currentBookmark.classList.contains("far")) {
-      currentBookmark.classList.replace("far", "fas");
-    } else {
-      currentBookmark.classList.replace("fas", "far");
-    }
-  });
-}
+// const bookmarkButtons = document.querySelectorAll(".bookmarks");
+// for (let i = 0; i < bookmarkButtons.length; i++) {
+//   const currentBookmark = bookmarkButtons[i];
+//   currentBookmark.addEventListener("click", () => {
+//     if (currentBookmark.classList.contains("far")) {
+//       currentBookmark.classList.replace("far", "fas");
+//     } else {
+//       currentBookmark.classList.replace("fas", "far");
+//     }
+//   });
+// }
